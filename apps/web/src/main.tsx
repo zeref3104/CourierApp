c3272398bbf655d5135157d2edb01fdfdc70b6fc
julio <julio@example.com>
2026-07-31 21:04:35 -0400

    feat(i18n): init i18n before app render

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store';
 import App from './App';
+import './i18n';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
